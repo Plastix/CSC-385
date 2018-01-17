60e429b54e6379668ff42c909111530b66c79cc8
Plastix <aidanpieper@gmail.com>
2018-01-16 19:10:30 -0500

    Almost have rasterize_triangle working. Point in triangle needs some more work.

--- js/rasterizer.js
+++ js/rasterizer.js
@@ -97,41 +97,141 @@ function rasterize_triangle(point1, point2, point3, color) {
 // Draws a filled triangle between the points
 // of the color. Implemented using flood fill.
 function rasterize_filled_triangle(point1, point2, point3, color) {
+    let is_in_bounds = (x, y) => x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT;
 
-    var points = [];
-    var colored = {};
+    let is_parallel = (par1, par2) => par1[0] * par2[1] - par2[0] * par1[1] === 0;
 
-    points = points.concat(rasterize_line(point1, point2, color));
-    points = points.concat(rasterize_line(point2, point3, color));
-    points = points.concat(rasterize_line(point3, point1, color));
+    let is_colored = (x, y) => colored[x][y];
 
-    var flood_fill = function (pixel) {
-        var x = pixel[0], y = pixel[1];
+    let color_pixel = (x, y) => colored[x][y] = true;
+
+    let slope = (p1, p2) => (p2[1] - p1[1]) / (p2[0] - p1[0]);
+
+    let is_on_segment = (p, e1, e2) => Math.max(e1[0], e2[0]) >= p[0] && p[0] >= Math.min(e1[0], e2[0])
+        && Math.max(e1[1], e2[1]) >= p[1] && p[1] >= Math.min(e1[1], e2[1]);
+
+    let get_random_point = () => vec2(
+        get_random_int(WIDTH * 10, WIDTH * 100),
+        get_random_int(0, HEIGHT * 10)
+    );
+
+    let get_neighboring_pixels = (p) => [
+        vec2(p[0] - 1, p[1]),
+        vec2(p[0] + 1, p[1]),
+        vec2(p[0], p[1] - 1),
+        vec2(p[0], p[1] + 1)
+    ];
+
+    let get_line_params = (p1, p2) => {
+        if (p1[0] === p2[0]) {
+            return [1, 0, p1[0]];
+        } else if (p1[1] === p2[1]) {
+            return [0, 1, p1[1]];
+        } else {
+            return [-slope(p1, p2), 1, p1[1] - (slope(p1, p2) * p1[0])];
+        }
+    };
+
+    // Assumes the lines are not parallel
+    let get_intersection = function (p1, p2, p3, p4) {
+        let params1 = get_line_params(p1, p2);
+        let params2 = get_line_params(p3, p4);
+
+        let a1 = params1[0];
+        let b1 = params1[1];
+        let c1 = params1[2];
+        let a2 = params2[0];
+        let b2 = params2[1];
+        let c2 = params2[2];
+
+        let denom = (a1 * b2 - a2 * b1);
+        let x = ((c1 * b2) - (c2 * b1)) / denom;
+        let y = ((c1 * a2) - (c2 * a1)) / denom;
+
+        return vec2(x, -y);
+    };
+
+
+    // From Mozilla documentation
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    let get_random_int = function (min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    };
+
+
+    let contains_point = function (point) {
+        let contained = false;
+
+        [[point1, point2], [point1, point3], [point2, point3]].forEach(function (line) {
+            let p1 = line[0];
+            let p2 = line[1];
+            let params2 = get_line_params(p1, p2);
+
+            // Generate a random point until we have a non-parallel line segment
+            let rand = get_random_point();
+            let params = get_line_params(point, rand);
+            while (is_parallel(params, params2)) {
+                rand = get_random_point();
+                params = get_line_params(point, rand);
+            }
+
+            let intersect = get_intersection(point, rand, p1, p2);
+            // console.log(point, rand, p1, p2, "intersect: ", intersect);
+
+            // Check whether intersection is on triangle edge
+            if (is_on_segment(intersect, p1, p2) && is_on_segment(intersect, point, rand)) {
+                contained = !contained;
+            }
+        });
+
+        if(contained){
+            console.log("point ", point, "inside of", point1, point2, point3);
+        }
+
+        return contained;
+    };
+
+    let flood_fill = function (x, y) {
         // Base case
-        if (colored[pixel] || x < 0 || x > WIDTH || y < 0 || y > WIDTH) {
+        if (!is_in_bounds(x, y) || is_colored(x, y)) {
             return;
         }
 
         write_pixel(x, y, color);
-        colored[pixel] = true;
+        color_pixel(x, y);
 
         // Recursively color
-        flood_fill(vec2(x + 1, y));
-        flood_fill(vec2(x - 1, y));
-        flood_fill(vec2(x, y + 1));
-        flood_fill(vec2(x, y - 1));
+        flood_fill(x + 1, y);
+        flood_fill(x - 1, y);
+        flood_fill(x, y + 1);
+        flood_fill(x, y - 1);
     };
 
-    var contains_point = function (pixel) {
-        // TODO (Aidan)
-        return true;
-    };
-
-    points.forEach(function (pixel) {
-        if (!colored[pixel] && contains_point(pixel)) {
-            flood_fill(pixel)
+    const colored = [];
+    for (let i = 0; i < HEIGHT; i++) {
+        for (let j = 0; j < WIDTH; j++) {
+            if (!colored[j]) {
+                colored[j] = [];
+            }
+            colored[j][i] = false;
         }
-    });
+    }
+
+    // Unnecessarily functional JS code booya!
+    rasterize_line(point1, point2, color)
+        .concat(rasterize_line(point2, point3, color))
+        .concat(rasterize_line(point3, point1, color))
+        .map(p => {
+                color_pixel(p[0], p[1]); // Side effects in a map - ouch
+                return get_neighboring_pixels(p);
+            }
+        )
+        .reduce((acc, x) => acc.concat(x)) // Flatten list
+        .filter(p => is_in_bounds(p[0], p[1]) && !is_colored(p[0], p[1]) && contains_point(p))
+        .forEach(p => write_pixel(p[0], p[1], vec3(0, 1, 1))) // TODO (Aidan) debug
+    // .forEach(p => flood_fill(p[0], p[1]));
 }
 
 // Takes an array of seven points given as vec2 in pixel
