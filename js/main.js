6923c7ed1357363ad84cbafa5a39003b882d0832
Plastix <aidanpieper@gmail.com>
2018-02-24 09:38:53 -0500

    Add OrbitControls and test lights

--- js/main.js
+++ js/main.js
@@ -5,10 +5,21 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+let controls = new THREE.OrbitControls(camera, renderer.domElement);
+
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const material = new THREE.MeshPhongMaterial({
+    color: 0x00ff00,
+    emissive: 0x00aa00,
+    specular: 0x111111,
+});
+
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+const light = new THREE.PointLight(0xffffff, 10, 100);
+light.position.set(5, 5, 0);
+scene.add(light);
 
 camera.position.z = 5;
 
@@ -17,13 +28,10 @@ stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
 document.body.appendChild(stats.dom);
 
 const animate = function () {
-    requestAnimationFrame(animate);
-
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
-
     renderer.render(scene, camera);
-    stats.update()
+    stats.update();
+    controls.update();
+    requestAnimationFrame(animate);
 };
 
-animate();
+requestAnimationFrame(animate);
