dd6eb9452ced4ee97f107f0d7704f316bb56fe29
Plastix <aidanpieper@gmail.com>
2018-03-10 13:14:31 -0500

    Make particles look a little bit better

--- js/ParticleSystem.js
+++ js/ParticleSystem.js
@@ -1,9 +1,10 @@
 let vertex_shader = `
     attribute float size;
+    attribute float alpha;
     varying vec4 vColor;
     
     void main() {
-        vColor = vec4(1.0, 1.0, 1.0, 1.0);
+        vColor = vec4(color, alpha);
         gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
         gl_PointSize = size;
     }
@@ -23,15 +24,16 @@ let fragment_shader = `
 const PARTICLE_GRAVITY = -0.5;
 
 class Particle {
-    constructor(pos, m, v, a, lifespan, max_size, min_size) {
+    constructor(pos, m, v, a, lifespan, color, size_bounds, alpha_bounds) {
         this.p = pos;
         this.m = m;
         this.v = v;
         this.a = a;
         this.age = 0;
         this.lifespan = lifespan;
-        this.max_size = max_size;
-        this.min_size = min_size;
+        this.color = color;
+        this.size_bounds = size_bounds;
+        this.alpha_bounds = alpha_bounds;
     }
 
     is_dead() {
@@ -79,8 +81,12 @@ class ParticleSystem {
     init_buffers() {
         this.pos_buffer = new THREE.BufferAttribute(new Float32Array(this.MAX_PARTICLES * 3), 3).setDynamic(true);
         this.geo.addAttribute("position", this.pos_buffer);
+        this.color_buffer = new THREE.BufferAttribute(new Float32Array(this.MAX_PARTICLES * 3), 3).setDynamic(true);
+        this.geo.addAttribute("color", this.color_buffer);
         this.size_buffer = new THREE.BufferAttribute(new Float32Array(this.MAX_PARTICLES), 1).setDynamic(true);
         this.geo.addAttribute("size", this.size_buffer);
+        this.alpha_buffer = new THREE.BufferAttribute(new Float32Array(this.MAX_PARTICLES), 1).setDynamic(true);
+        this.geo.addAttribute("alpha", this.alpha_buffer);
     }
 
     render() {
@@ -130,15 +136,24 @@ class ParticleSystem {
         let j = 0;
         for (let i = 0; i < this.particles.length; i++) {
             let particle = this.particles[i];
-            this.pos_buffer.array[j++] = particle.p.x;
-            this.pos_buffer.array[j++] = particle.p.y;
-            this.pos_buffer.array[j++] = particle.p.z;
+            this.pos_buffer.array[j] = particle.p.x;
+            this.color_buffer.array[j] = particle.color.x;
+            j++;
+            this.pos_buffer.array[j] = particle.p.y;
+            this.color_buffer.array[j] = particle.color.y;
+            j++;
+            this.pos_buffer.array[j] = particle.p.z;
+            this.color_buffer.array[j] = particle.color.z;
+            j++;
 
-            this.size_buffer.array[i] = lerp(particle.max_size, particle.min_size, particle.age / particle.lifespan);
+            this.size_buffer.array[i] = lerp(particle.size_bounds.x, particle.size_bounds.y, particle.age / particle.lifespan);
+            this.alpha_buffer.array[i] = lerp(particle.alpha_bounds.x, particle.alpha_bounds.y, particle.age / particle.lifespan);
         }
 
         this.pos_buffer.needsUpdate = true;
         this.size_buffer.needsUpdate = true;
+        this.alpha_buffer.needsUpdate = true;
+        this.color_buffer.needsUpdate = true;
     }
 
     get_object3D() {
@@ -169,8 +184,9 @@ class Emitter {
                 new THREE.Vector3(getRandomArbitrary(-0.2, 0.2), getRandomArbitrary(0, 0.3), getRandomArbitrary(-0.2, 0.2)),
                 new THREE.Vector3(0, PARTICLE_GRAVITY, 0),
                 1.5,
-                50,
-                20));
+                new THREE.Vector3(getRandomArbitrary(0, 1), getRandomArbitrary(0, 1), getRandomArbitrary(0, 1)),
+                new THREE.Vector2(50, 20),
+                new THREE.Vector2(1, 0)));
         }
     }
 
